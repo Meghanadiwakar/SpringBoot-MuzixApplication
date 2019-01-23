0257f86547018ad480693bc50c5449f323a59c08
meghanadiwakar <meghana567naidu@gmail.com>
2019-01-23 10:25:15 +0530

    application listener and command line initial commit

--- src/main/java/com/stackroute/MuzixApplication.java
+++ src/main/java/com/stackroute/MuzixApplication.java
@@ -1,14 +1,45 @@
 package com.stackroute;
 
+import com.stackroute.domain.Track;
+import com.stackroute.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.hibernate.SpringJtaPlatform;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+
+@Component
 @SpringBootApplication
-public class MuzixApplication {
+public class MuzixApplication implements ApplicationListener<ContextRefreshedEvent>, CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MuzixApplication.class, args);
+	@Autowired
+	TrackRepository trackRepository;
+	public static void main(String args[]){
+		SpringApplication.run(MuzixApplication.class,args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		trackRepository.save(new Track(2,"hello","lionel"));
+
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		trackRepository.save(new Track(1,"buffalo","bob marley"));
+
+
+	}
+
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(MuzixApplication.class, args);
+//	}
+
 
 }
 
