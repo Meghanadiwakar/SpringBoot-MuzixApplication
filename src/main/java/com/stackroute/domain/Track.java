bd1783e0764f48d200f0d4829a96f18b7c429774
meghanadiwakar <meghana567naidu@gmail.com>
2019-01-23 09:25:54 +0530

    lombok initial commit

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,55 +1,23 @@
 package com.stackroute.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Track {
     @Id
     private int trackId;
     private String trackName;
     private String comment;
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "trackId=" + trackId +
-                ", trackname='" + trackName + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
-
-    public Track(int trackId, String trackName, String comment) {
-        this.trackId = trackId;
-        this.trackName = trackName;
-        this.comment = comment;
-    }
-
-    public Track() {
-    }
-
-    public int getTrackId() {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId) {
-        this.trackId = trackId;
-    }
-
-    public String getTrackname() {
-        return trackName;
-    }
-
-    public void setTrackname(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
 }
